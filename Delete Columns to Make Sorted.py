a9aaf065108bb136ceb86fb291e6988f95ff6919
Honey Tyagi <honeyt290@gmail.com>
2023-01-03 23:04:28 +0530

    commit

---
+++ Delete Columns to Make Sorted.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        counter = 0
+        for i in zip(*strs):
+            if list(i) != sorted(i):
+                counter += 1
+        return counter
