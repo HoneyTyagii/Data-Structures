bf6e635269e350a560d2715ce7ad33812ee9335c
Honey Tyagi <honeyt290@gmail.com>
2023-01-03 10:58:32 +0530

    Detect Capital

--- Detect Capital.py
+++ Detect Capital.py
@@ -23,15 +23,44 @@
 # word consists of lowercase and uppercase English letters.
 
 
+# Approach
+
+# If the string is empty, return True.
+
+# If the string has only one character, return True.
+
+# If the first character is uppercase and the rest of the characters are lowercase, return True.
+
+# If the first character is lowercase and the rest of the characters are uppercase, return False.
+
+# If the first character is uppercase and the rest of the characters are uppercase, return True.
+
+# If the first character is lowercase and the rest of the characters are lowercase, return True.
+
+# Complexity
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+#code
+
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
+    # If the string is empty or has only one character, return True
         if len(word) <= 1:
             return True
+    
+    # If the first character is uppercase and the rest of the characters are lowercase, return True
         if word[0].isupper() and word[1:].islower():
             return True
+    
+    # If the first character is lowercase and the rest of the characters are uppercase, return False
         if word[0].islower() and word[1:].isupper():
             return False
+    
+    # If the first character is uppercase and the rest of the characters are uppercase, return True
         if word[0].isupper() and word[1:].isupper():
             return True
+    
+    # If the first character is lowercase and the rest of the characters are lowercase, return True
         if word[0].islower() and word[1:].islower():
             return True
