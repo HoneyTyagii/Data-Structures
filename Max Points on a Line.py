05b70b5ddf2f47c2a507e4f1ee2e0cb469e1f794
Honey Tyagi <honeyt290@gmail.com>
2023-01-10 23:28:34 +0530

    commit

--- Max Points on a Line.py
+++ Max Points on a Line.py
@@ -10,7 +10,7 @@
 
 # Input: points = [[1,1],[2,2],[3,3]]
 # Output: 3
-# Example 2:
+# Example 2
 
 # Input: points = [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
 # Output: 4
@@ -20,7 +20,7 @@
 # 1 <= points.length <= 300
 # points[i].length == 2
 # -104 <= xi, yi <= 104
-# All the points are unique.
+# All the points are unique
 
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
