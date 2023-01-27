dc6934d437cb0486f98d651185541ea4953941a0
Honey Tyagi <honeyt290@gmail.com>
2023-01-27 20:50:50 +0530

    commit

--- Concatenated Words.py
+++ Concatenated Words.py
@@ -26,3 +26,24 @@
 # words[i] consists of only lowercase English letters.
 # All the strings of words are unique.
 # 1 <= sum(words[i].length) <= 105
+
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        def can(w,dit):
+            for i in range(mini,len(w)):
+                lf=w[:i]
+                rt=w[i:]
+                if lf in dit:
+                    if rt in dit or can(rt,dit):
+                        return True
+            # return False
+        res=[]
+        dit = set(list(words))
+        mini=10000
+        for w in words:
+            mini=min(len(w),mini)
+        for w in words:
+            if can(w,dit):
+                res.append(w)
+                
+        return res
