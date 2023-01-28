f7eed3e903d41d42a3cdf9065f1cf76f04198524
Honey Tyagi <honeyt290@gmail.com>
2023-01-28 23:10:45 +0530

    commit

--- Data Stream as Disjoint Intervals.py
+++ Data Stream as Disjoint Intervals.py
@@ -8,7 +8,7 @@
 # void addNum(int value) Adds the integer value to the stream.
 # int[][] getIntervals() Returns a summary of the integers in the stream currently as a list of disjoint intervals [starti, endi]. The answer should be sorted by starti.
                 
-            
+                              
 # Example 1:          
                                
 # Input
