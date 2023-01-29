141d14b03d037f2af30a69be4d814169e8b4ebfc
Honey Tyagi <honeyt290@gmail.com>
2023-01-29 20:52:50 +0530

    commit

--- Data Stream as Disjoint Intervals.py
+++ Data Stream as Disjoint Intervals.py
@@ -1,72 +1,56 @@
-# 352. Data Stream as Disjoint Intervals
-               
-# Given a data stream input of non-negative integers a1, a2, ..., an, summarize the numbers seen so far as a list of disjoint intervals.
-              
-# Implement the SummaryRanges class:             
-                     
-# SummaryRanges() Initializes the object with an empty stream.
-# void addNum(int value) Adds the integer value to the stream.
-# int[][] getIntervals() Returns a summary of the integers in the stream currently as a list of disjoint intervals [starti, endi]. The answer should be sorted by start i.            
-                
-                              
-# Example 1:          
-                               
+# 460. LFU Cache
+# Hard
+# 4.5K
+# 278
+# Companies
+# Design and implement a data structure for a Least Frequently Used (LFU) cache.
+
+# Implement the LFUCache class:
+
+# LFUCache(int capacity) Initializes the object with the capacity of the data structure.
+# int get(int key) Gets the value of the key if the key exists in the cache. Otherwise, returns -1.
+# void put(int key, int value) Update the value of the key if present, or inserts the key if not already present. When the cache reaches its capacity, it should invalidate and remove the least frequently used key before inserting a new item. For this problem, when there is a tie (i.e., two or more keys with the same frequency), the least recently used key would be invalidated.
+# To determine the least frequently used key, a use counter is maintained for each key in the cache. The key with the smallest use counter is the least frequently used key.
+
+# When a key is first inserted into the cache, its use counter is set to 1 (due to the put operation). The use counter for a key in the cache is incremented either a get or put operation is called on it.
+
+# The functions get and put must each run in O(1) average time complexity.
+
+ 
+
+# Example 1:
+
 # Input
-# ["SummaryRanges", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals"]
-# [[], [1], [], [3], [], [7], [], [2], [], [6], []]
+# ["LFUCache", "put", "put", "get", "put", "get", "get", "put", "get", "get", "get"]
+# [[2], [1, 1], [2, 2], [1], [3, 3], [2], [3], [4, 4], [1], [3], [4]]
 # Output
-# [null, null, [[1, 1]], null, [[1, 1], [3, 3]], null, [[1, 1], [3, 3], [7, 7]], null, [[1, 3], [7, 7]], null, [[1, 3], [6, 7]]]
+# [null, null, null, 1, null, -1, 3, null, -1, 3, 4]
 
 # Explanation
-# SummaryRanges summaryRanges = new SummaryRanges();
-# summaryRanges.addNum(1);      // arr = [1]
-# summaryRanges.getIntervals(); // return [[1, 1]]
-# summaryRanges.addNum(3);      // arr = [1, 3]
-# summaryRanges.getIntervals(); // return [[1, 1], [3, 3]]
-# summaryRanges.addNum(7);      // arr = [1, 3, 7]
-# summaryRanges.getIntervals(); // return [[1, 1], [3, 3], [7, 7]]
-# summaryRanges.addNum(2);      // arr = [1, 2, 3, 7]
-# summaryRanges.getIntervals(); // return [[1, 3], [7, 7]]
-# summaryRanges.addNum(6);      // arr = [1, 2, 3, 6, 7]
-# summaryRanges.getIntervals(); // return [[1, 3], [6, 7]]
+# // cnt(x) = the use counter for key x
+# // cache=[] will show the last used order for tiebreakers (leftmost element is  most recent)
+# LFUCache lfu = new LFUCache(2);
+# lfu.put(1, 1);   // cache=[1,_], cnt(1)=1
+# lfu.put(2, 2);   // cache=[2,1], cnt(2)=1, cnt(1)=1
+# lfu.get(1);      // return 1
+#                  // cache=[1,2], cnt(2)=1, cnt(1)=2
+# lfu.put(3, 3);   // 2 is the LFU key because cnt(2)=1 is the smallest, invalidate 2.
+#                  // cache=[3,1], cnt(3)=1, cnt(1)=2
+# lfu.get(2);      // return -1 (not found)
+# lfu.get(3);      // return 3
+#                  // cache=[3,1], cnt(3)=2, cnt(1)=2
+# lfu.put(4, 4);   // Both 1 and 3 have the same cnt, but 1 is LRU, invalidate 1.
+#                  // cache=[4,3], cnt(4)=1, cnt(3)=2
+# lfu.get(1);      // return -1 (not found)
+# lfu.get(3);      // return 3
+#                  // cache=[3,4], cnt(4)=1, cnt(3)=3
+# lfu.get(4);      // return 4
+#                  // cache=[4,3], cnt(4)=2, cnt(3)=3
  
 
 # Constraints:
 
-# 0 <= value <= 104
-# At most 3 * 104 calls will be made to addNum and getIntervals.
-
-class SummaryRanges:
-
-	def __init__(self):
-		self.set = set()
-
-	def addNum(self, value: int) -> None:
-		self.set.add(value)
-
-	def getIntervals(self) -> List[List[int]]:
-
-		result = []
-		index = 1
-		array = sorted(list(self.set))
-		
-		start = array[0]
-		current_element = array[0]
-
-		if len(array) == 1:
-			result.append([start, start])
-			return result
-
-		while index < len(array):
-
-			if array[index] != current_element + 1:
-				result.append([start, array[index - 1]])
-				start = array[index]
-
-			current_element = array[index]
-			index = index + 1
-
-		result.append([start,array[index - 1]])
-
-		return result 
-		
+# 0 <= capacity <= 104
+# 0 <= key <= 105
+# 0 <= value <= 109
+# At most 2 * 105 calls will be made to get and put.
