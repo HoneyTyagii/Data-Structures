5545192fbcea2e6b3eef0d63e78bce9a46b840a2
Honey Tyagi <honeyt290@gmail.com>
2023-01-31 21:32:10 +0530

    commit

--- Best Team With No Conflicts.py
+++ Best Team With No Conflicts.py
@@ -31,3 +31,14 @@
 # scores.length == ages.length
 # 1 <= scores[i] <= 106
 # 1 <= ages[i] <= 1000
+
+class Solution:
+    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+                                               
+        dp = [0]*(1+max(ages))                     
+        score_age = sorted(zip(scores, ages))
+                                               
+        for score, age in score_age:            
+            dp[age] = score + max(dp[:age+1])  
+            
+        return max(dp)         
