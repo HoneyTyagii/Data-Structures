050bf257e56633e54c84c51b8094833eee49070b
Honey Tyagi <honeyt290@gmail.com>
2023-02-01 22:54:24 +0530

    commit

--- Greatest Common Divisor of Strings.py
+++ Greatest Common Divisor of Strings.py
@@ -24,3 +24,8 @@
 
 # 1 <= str1.length, str2.length <= 1000
 # str1 and str2 consist of English uppercase letters.
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        def gcd(a, b):  return abs(a) if b == 0 else gcd(b, a%b)
+        return str1[:gcd(len(str1), len(str2))] if (str1 + str2 == str2 + str1) else ''
