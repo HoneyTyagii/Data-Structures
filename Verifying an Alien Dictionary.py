576d2cf4f297135c12b10d808ff82b0dfdb12f6f
Honey Tyagi <honeyt290@gmail.com>
2023-02-02 22:06:07 +0530

    commit

--- Verifying an Alien Dictionary.py
+++ Verifying an Alien Dictionary.py
@@ -29,3 +29,20 @@
 # 1 <= words[i].length <= 20
 # order.length == 26
 # All characters in words[i] and order are English lowercase letters.
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_index = {c: i for i, c in enumerate(order)}
+        for i in range(len(words) - 1):
+            word1 = words[i]
+            word2 = words[i + 1]
+            for j in range(min(len(word1), len(word2))):
+                if word1[j] != word2[j]:
+                    if order_index[word1[j]] > order_index[word2[j]]:
+                        return False
+                    break
+            else:
+                if len(word1) > len(word2):
+                    return False
+        return True
+         
