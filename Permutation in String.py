7f4169e0087139c4b976d6cce56f724e3e6c9dd5
Honey Tyagi <honeyt290@gmail.com>
2023-02-04 23:17:26 +0530

    commit

--- Permutation in String.py
+++ Permutation in String.py
@@ -21,3 +21,22 @@
 
 # 1 <= s1.length, s2.length <= 104
 # s1 and s2 consist of lowercase English letters.
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        cntr, w, matched = Counter(s1), len(s1), 0   
+
+        for i in range(len(s2)):
+            if s2[i] in cntr: 
+                cntr[s2[i]] -= 1
+                if cntr[s2[i]] == 0:
+                    matched += 1
+            if i >= w and s2[i-w] in cntr: 
+                if cntr[s2[i-w]] == 0:
+                    matched -= 1
+                cntr[s2[i-w]] += 1
+
+            if matched == len(cntr):
+                return True
+
+        return False
