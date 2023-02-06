89b52b8f69cbc6f103e396d9a935e9429d56c1ef
Honey Tyagi <honeyt290@gmail.com>
2023-02-06 23:08:55 +0530

    commit

--- Jump Game.py
+++ Jump Game.py
@@ -22,3 +22,11 @@
 
 # 1 <= nums.length <= 104
 # 0 <= nums[i] <= 105
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        last_good_position = len(nums) - 1
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= last_good_position:
+                last_good_position = i
+        return last_good_position == 0
