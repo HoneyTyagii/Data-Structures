8529985f88bdeb508b5cac612150d234b6a6dc53
Honey Tyagi <honeyt290@gmail.com>
2023-02-13 22:53:37 +0530

    commit

--- Count Odd Numbers in an Interval Range.py
+++ Count Odd Numbers in an Interval Range.py
@@ -19,3 +19,11 @@
 # Constraints:
 
 # 0 <= low <= high <= 10^9
+
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        if low % 2 == 0:
+            low += 1
+        if high % 2 == 0:
+            high -=1
+        return (high - low) // 2 + 1 if low <= high else 0
