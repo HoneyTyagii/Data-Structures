a054c77af11bb66ad21423f6b968d805d809d115
Honey Tyagi <honeyt290@gmail.com>
2023-02-14 22:50:57 +0530

    commit

--- Add Binary.py
+++ Add Binary.py
@@ -19,3 +19,24 @@
 # 1 <= a.length, b.length <= 104
 # a and b consist only of '0' or '1' characters.
 # Each string does not contain leading zeros except for the zero itself.
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        # Initialize the result string and the carry value
+        result = ""
+        carry = 0
+        # Convert the input strings to lists of digits
+        a = list(a)
+        b = list(b)
+        # Loop until both lists are empty or the carry is nonzero
+        while a or b or carry:
+            # Pop the rightmost digits from the lists, or use zero if empty
+            x = int(a.pop()) if a else 0
+            y = int(b.pop()) if b else 0
+            # Add the digits and the carry, and update the carry
+            sum = x + y + carry
+            carry = sum // 2
+            # Append the remainder to the result string
+            result = str(sum % 2) + result
+        # Return the result string
+        return result
