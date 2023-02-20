da0ab77f06e63fb32dc627b91548ec7cecd3a40b
Honey Tyagi <honeyt290@gmail.com>
2023-02-20 22:42:02 +0530

    commit

--- Zigzag Conversion.py
+++ Zigzag Conversion.py
@@ -39,7 +39,7 @@
  
 class Solution:
     def convert(self, s: str, numRows: int) -> str:  
-        if numRows==1: return s 
+        if numRows==1: return s  
         res = ["" for i in range(numRows)]    
         i = 0  
         direction = -1   
