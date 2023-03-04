53c00afd4bc43669cfd73c352ba3bf4f8fb0036c
Honey Tyagi <honeyt290@gmail.com>
2023-03-04 21:46:55 +0530

    commit

--- Detect Capital.py
+++ Detect Capital.py
@@ -1,6 +1,6 @@
 # 520. Detect Capital
  
-# All letters in this word are capitals, like "USA".
+# All letters in this word are capitals, like "USA". 
 # All letters in this word are not capitals, like "leetcode".
 # Only the first letter in this word is capital, like "Google".  
 # Given a string word, return true if the usage of capitals in it is right.  
