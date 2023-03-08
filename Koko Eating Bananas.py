9928a8fada2094ba372feedf1b1edca194eacaf5
Honey Tyagi <honeyt290@gmail.com>
2023-03-08 21:20:19 +0530

    commit

--- Koko Eating Bananas.py
+++ Koko Eating Bananas.py
@@ -1,34 +1,34 @@
-875. Koko Eating Bananas
-Medium
-6.9K
-324
-Companies
-Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+# 875. Koko Eating Bananas
+# Medium
+# 6.9K
+# 324
+# Companies
+# Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
 
-Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+# Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
 
-Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+# Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
 
-Return the minimum integer k such that she can eat all the bananas within h hours.
+# Return the minimum integer k such that she can eat all the bananas within h hours.
 
  
 
-Example 1:
+# Example 1:
 
-Input: piles = [3,6,7,11], h = 8
-Output: 4
-Example 2:
+# Input: piles = [3,6,7,11], h = 8
+# Output: 4
+# Example 2:
 
-Input: piles = [30,11,23,4,20], h = 5
-Output: 30
-Example 3:
+# Input: piles = [30,11,23,4,20], h = 5
+# Output: 30
+# Example 3:
 
-Input: piles = [30,11,23,4,20], h = 6
-Output: 23
+# Input: piles = [30,11,23,4,20], h = 6
+# Output: 23
  
 
-Constraints:
+# Constraints:
 
-1 <= piles.length <= 104
-piles.length <= h <= 109
-1 <= piles[i] <= 109
+# 1 <= piles.length <= 104
+# piles.length <= h <= 109
+# 1 <= piles[i] <= 109
