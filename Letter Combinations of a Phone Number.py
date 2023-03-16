673324c4037cc67c068da903d2d734d58fff72ed
Honey Tyagi <honeyt290@gmail.com>
2023-03-16 23:08:38 +0530

    commit

--- Letter Combinations of a Phone Number.py
+++ Letter Combinations of a Phone Number.py
@@ -2,7 +2,6 @@
 # Medium
 # 14.4K
 # 823
-# Companies
 # Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
 
 # A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
