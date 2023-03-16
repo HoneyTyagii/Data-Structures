e9342bc0304f5834322e594e9401d66afd60c082
Honey Tyagi <honeyt290@gmail.com>
2023-03-16 23:11:17 +0530

    commit

--- Letter Combinations of a Phone Number.py
+++ Letter Combinations of a Phone Number.py
@@ -25,3 +25,25 @@
 
 # 0 <= digits.length <= 4
 # digits[i] is a digit in the range ['2', '9'].
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}
+        res = []
+        def dfs(i, path):
+            if i == len(digits):
+                res.append(path)
+                return
+            for j in phone[digits[i]]:
+                dfs(i+1, path+j)
+        dfs(0, '')
+        return res
