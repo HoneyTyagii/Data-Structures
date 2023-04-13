3fa32ff422f640c245370e86af1d41478bbfd2fd
Honey Tyagi <honeyt290@gmail.com>
2023-04-13 23:39:46 +0530

    commit

--- Validate Stack Sequences.py
+++ Validate Stack Sequences.py
@@ -27,3 +27,14 @@
 # All the elements of pushed are unique.
 # popped.length == pushed.length
 # popped is a permutation of pushed.
+
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack=[]
+        i=0
+        for num in pushed:
+            stack.append(num) 
+            while  len(stack) > 0 and stack[len(stack)-1] == popped[i] :
+                stack.pop()
+                i+=1 
+        return True if len(stack) ==0 else False
