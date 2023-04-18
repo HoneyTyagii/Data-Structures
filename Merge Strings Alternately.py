7394239faef56b73f4fc8f6c5e510640edb8c8d2
Honey Tyagi <honeyt290@gmail.com>
2023-04-18 23:43:46 +0530

    commit

--- Merge Strings Alternately.py
+++ Merge Strings Alternately.py
@@ -36,3 +36,15 @@
 
 # 1 <= word1.length, word2.length <= 100
 # word1 and word2 consist of lowercase English letters.
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        result = []
+        i = 0
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
+            i += 1
+        return ''.join(result)
