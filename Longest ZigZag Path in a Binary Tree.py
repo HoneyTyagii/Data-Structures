1a91df708c5a1a043f4d7be8dccf5d0fcf34fa76
Honey Tyagi <honeyt290@gmail.com>
2023-04-19 23:14:11 +0530

    commit

--- Longest ZigZag Path in a Binary Tree.py
+++ Longest ZigZag Path in a Binary Tree.py
@@ -36,3 +36,22 @@
 
 # The number of nodes in the tree is in the range [1, 5 * 104].
 # 1 <= Node.val <= 100
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def longestZigZag(self, root):
+        ans = 0
+        stack = [(root, 0, None)]
+        while stack:
+            node, n, left = stack.pop()
+            if node:
+                ans = max(ans, n)
+                stack.append((node.left, 1 if left else n + 1, 1))
+                stack.append((node.right, n + 1 if left else 1, 0))
+        return ans
