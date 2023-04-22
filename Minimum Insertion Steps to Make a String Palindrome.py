d5cc5ef5aedc59b2ea76abe5855444b243c7a26c
Honey Tyagi <honeyt290@gmail.com>
2023-04-22 21:52:30 +0530

    commit

--- Minimum Insertion Steps to Make a String Palindrome.py
+++ Minimum Insertion Steps to Make a String Palindrome.py
@@ -29,3 +29,18 @@
 
 # 1 <= s.length <= 500
 # s consists of lowercase English letters.
+
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        n = len(s)
+        dp = [0] * n
+        for i in range(n - 2, -1, -1):
+            prev = 0
+            for j in range(i + 1, n):
+                temp = dp[j]
+                if s[i] == s[j]:
+                    dp[j] = prev
+                else:
+                    dp[j] = min(dp[j], dp[j-1]) + 1
+                prev = temp
+        return dp[n-1]
