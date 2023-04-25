c62fb47ed243b53feca7fa4c73ba4f6128f712de
Honey Tyagi <honeyt290@gmail.com>
2023-04-25 22:49:56 +0530

    commit

--- Smallest Number in Infinite Set.py
+++ Smallest Number in Infinite Set.py
@@ -34,3 +34,25 @@
 
 # 1 <= num <= 1000
 # At most 1000 calls will be made in total to popSmallest and addBack.
+
+import heapq
+class SmallestInfiniteSet:
+    def __init__(self):
+        self.pq = []
+        for i in range(1, 1001):
+            heapq.heappush(self.pq, i)
+
+    def popSmallest(self):
+        if self.pq:
+            return heapq.heappop(self.pq)
+        return -1
+
+    def addBack(self, num):
+        if num not in self.pq:
+            heapq.heappush(self.pq, num)
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
