433ecb914c1ceec9c090d33bcf18f31f5ce451bb
Honey Tyagi <honeyt290@gmail.com>
2023-05-07 22:39:20 +0530

    commit

--- Find the Longest Valid Obstacle Course at Each Position.py
+++ Find the Longest Valid Obstacle Course at Each Position.py
@@ -47,3 +47,19 @@
 # n == obstacles.length
 # 1 <= n <= 105
 # 1 <= obstacles[i] <= 107
+
+class Solution:
+    def longestObstacleCourseAtEachPosition(self, obstacles: List[int]) -> List[int]:
+        n = len(obstacles)
+        dp = []
+        tails = []
+        for obstacle in obstacles:
+            if not dp or obstacle >= dp[-1]:
+                dp.append(obstacle)
+                tails.append(len(dp) - 1)
+            else:
+                idx = bisect_right(dp, obstacle)
+                dp[idx] = obstacle
+                tails.append(idx)
+        tails = [x+1 for x in tails]
+        return tails
