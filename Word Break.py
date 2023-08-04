f53a62d9745659a4eed6a473103b7aeffbc7ae4f
Honey Tyagi <honeyt290@gmail.com>
2023-08-04 21:28:35 +0530

    commit

---
+++ Word Break.py
@@ -0,0 +1,48 @@
+# 139. Word Break
+
+# Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+# Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+ 
+
+# Example 1:
+
+# Input: s = "leetcode", wordDict = ["leet","code"]
+# Output: true
+# Explanation: Return true because "leetcode" can be segmented as "leet code".
+# Example 2:
+
+# Input: s = "applepenapple", wordDict = ["apple","pen"]
+# Output: true
+# Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+# Note that you are allowed to reuse a dictionary word.
+# Example 3:
+
+# Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+# Output: false
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 300
+# 1 <= wordDict.length <= 1000
+# 1 <= wordDict[i].length <= 20
+# s and wordDict[i] consist of only lowercase English letters.
+# All the strings of wordDict are unique.
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        visited = set([s])
+        while len(visited) > 0:
+            word = visited.pop()
+            if word in wordDict:
+                return True
+            else:
+                for w in wordDict:
+                    if word.find(w) == 0:
+                        visited.add(word[len(w):])
+        return False
+    
+    # time complexity: O(n^2)
+    # space complexity: O(n)
