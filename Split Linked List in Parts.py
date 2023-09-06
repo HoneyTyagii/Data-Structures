e2f683343b57e95a791dce405ae80ca155a2ca55
Honey Tyagi <honeyt290@gmail.com>
2023-09-06 21:32:33 +0530

    commit

---
+++ Split Linked List in Parts.py
@@ -0,0 +1,73 @@
+# 725. Split Linked List in Parts
+
+# Medium
+
+# Given the head of a singly linked list and an integer k, split the linked list into k consecutive linked list parts.
+
+# The length of each part should be as equal as possible: no two parts should have a size differing by more than one. This may lead to some parts being null.
+
+# The parts should be in the order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal to parts occurring later.
+
+# Return an array of the k parts.
+
+# Example 1:
+
+# Input: head = [1,2,3], k = 5
+# Output: [[1],[2],[3],[],[]]
+# Explanation:
+# The first element output[0] has output[0].val = 1, output[0].next = null.
+# The last element output[4] is null, but its string representation as a ListNode is [].
+# Example 2:
+
+# Input: head = [1,2,3,4,5,6,7,8,9,10], k = 3
+# Output: [[1,2,3,4],[5,6,7],[8,9,10]]
+# Explanation:
+# The input has been split into consecutive parts with size difference at most 1, and earlier parts are a larger size than the later parts.
+
+# Constraints:
+
+# The number of nodes in the list is in the range [0, 1000].
+# 0 <= Node.val <= 1000
+# 1 <= k <= 50
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
+        current = head
+        temp = []
+        result = []
+        n = 0
+        index = 0
+        
+        while current:
+            temp.append(current.val)
+            current = current.next
+            n += 1
+        
+        while k > 0:
+            size = n // k
+            
+            if n % k > 0:
+                size += 1
+                
+            current = ListNode()
+            head = current
+            
+            for _ in range(size):
+                node = ListNode(temp[index])
+                current.next = node
+                current = current.next
+                index += 1
+                n -= 1
+                
+            result.append(head.next)
+            k -= 1
+                
+        return result
+
+# time complexity : O(n)
+# space complexity : O(n)
