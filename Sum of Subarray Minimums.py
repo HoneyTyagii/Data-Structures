b23ed546a0505e92c38d3aa1a9fbda44f179387f
Honey Tyagi <honeyt290@gmail.com>
2024-01-20 11:43:59 +0530

    commit

---
+++ Sum of Subarray Minimums.py
@@ -0,0 +1,45 @@
+# 907. Sum of Subarray Minimums
+
+# Medium
+
+# Given an array of integers arr, find the sum of min(b), where b ranges over every (contiguous) subarray of arr. Since the answer may be large, return the answer modulo 109 + 7. 
+
+# Example 1:
+
+# Input: arr = [3,1,2,4]
+# Output: 17
+# Explanation: 
+# Subarrays are [3], [1], [2], [4], [3,1], [1,2], [2,4], [3,1,2], [1,2,4], [3,1,2,4]. 
+# Minimums are 3, 1, 2, 4, 1, 1, 2, 1, 1, 1.
+# Sum is 17.
+# Example 2:
+
+# Input: arr = [11,81,94,43,3]
+# Output: 444
+ 
+
+# Constraints:
+
+# 1 <= arr.length <= 3 * 104
+# 1 <= arr[i] <= 3 * 104
+
+class Solution:
+    def sumSubarrayMins(self, A: List[int]) -> int:
+        
+        A = [-math.inf] + A + [-math.inf]
+        n = len(A)
+        
+        st = []
+        res = 0
+        
+        for i in range(n):
+            
+            while st and A[st[-1]] > A[i]: 
+                mid = st.pop()
+                left = st[-1] 
+                right = i 
+                
+                res += A[mid] * (mid - left) * (right - mid)
+            
+            st.append(i)
+        return res %(10**9 + 7)
