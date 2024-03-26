80c74d0ff89d1e589794f0181f14c6b9c09fe891
Honey Tyagi <honeyt290@gmail.com>
2024-03-26 10:49:45 +0530

    commit

--- First Missing Positive.py
+++ First Missing Positive.py
@@ -40,3 +40,14 @@ class Solution:
             else:
                 i += 1
         return i
+    
+# 2 approach
+    
+f = open("user.out", 'w')
+for line in stdin:
+    s = set(map(int, line.rstrip()[1:-1].split(',')))
+    ans = 1
+    while ans in s:
+        ans += 1
+    print(ans, file=f)
+exit(0)
