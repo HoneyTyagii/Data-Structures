8cdab664a0231159dd22073b2d54de75efb9d7ec
Honey Tyagi <honeyt290@gmail.com>
2024-04-14 09:27:07 +0530

    commit

---
+++ Sum of Left Leaves.py
@@ -0,0 +1,34 @@
+# 404. Sum of Left Leaves
+
+# Easy
+
+# Given the root of a binary tree, return the sum of all left leaves.
+
+# A leaf is a node with no children. A left leaf is a leaf that is the left child of another node. 
+
+# Example 1:
+
+# Input: root = [3,9,20,null,null,15,7]
+# Output: 24
+# Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+# Example 2:
+
+# Input: root = [1]
+# Output: 0
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [1, 1000].
+# -1000 <= Node.val <= 1000
+
+class Solution:
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        def traverse(node, is_left):
+            if not node:
+                return 0
+            if not node.left and not node.right and is_left:
+                return node.val
+            left_sum = traverse(node.left, True)
+            right_sum = traverse(node.right, False)
+            return left_sum + right_sum
+        return traverse(root, False)
