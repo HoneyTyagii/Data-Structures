1f7abbb5e87fff7674c5ea3ccf5110596f790c85
Honey Tyagi <honeyt290@gmail.com>
2024-05-21 18:53:47 +0530

    commit

--- Subsets.py
+++ Subsets.py
@@ -40,3 +40,17 @@ class Solution:
     
         # time complexity: O(n^2)
         # space complexity: O(n^2)
+    
+
+# 2 Approach
+    
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start, path):
+            result.append(path)
+            for i in range(start, len(nums)):
+                backtrack(i + 1, path + [nums[i]])
+
+        result = []
+        backtrack(0, [])
+        return result
