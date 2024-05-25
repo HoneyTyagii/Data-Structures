12521c84dc0d64857e06d42aba7d18ad30c76f1f
Honey Tyagi <honeyt290@gmail.com>
2024-05-25 10:19:47 +0530

    commit

---
+++ Word Break II.py
@@ -0,0 +1,50 @@
+# 140. Word Break II
+
+# Hard
+
+# Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
+
+# Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+# Example 1:
+
+# Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+# Output: ["cats and dog","cat sand dog"]
+
+# Example 2:
+
+# Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
+# Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+# Explanation: Note that you are allowed to reuse a dictionary word.
+
+# Example 3:
+
+# Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+# Output: [] 
+
+# Constraints:
+
+# 1 <= s.length <= 20
+# 1 <= wordDict.length <= 1000
+# 1 <= wordDict[i].length <= 10
+# s and wordDict[i] consist of only lowercase English letters.
+# All the strings of wordDict are unique.
+# Input is generated in a way that the length of the answer doesn't exceed 105.
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordSet = set(wordDict)
+        memo = defaultdict(list)
+        def _wordBreak_topdown(s):
+            if not s:
+                return [[]]  
+            if s in memo:
+                return memo[s]
+            for endIndex in range(1, len(s) + 1):
+                word = s[:endIndex]
+                if word in wordSet:
+                    for subsentence in _wordBreak_topdown(s[endIndex:]):
+                        memo[s].append([word] + subsentence)
+            return memo[s]
+        _wordBreak_topdown(s)
+        return [" ".join(words) for words in memo[s]]
