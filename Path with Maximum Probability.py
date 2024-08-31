47f3752067a55ff5d53cc42660e22c216da0d83c
Honey Tyagi <honeyt290@gmail.com>
2024-08-31 20:16:58 +0530

    commit

--- Path with Maximum Probability.py
+++ Path with Maximum Probability.py
@@ -6,29 +6,22 @@
 
 # If there is no path from start to end, return 0. Your answer will be accepted if it differs from the correct answer by at most 1e-5.
 
- 
-
 # Example 1:
-
-
 
 # Input: n = 3, edges = [[0,1],[1,2],[0,2]], succProb = [0.5,0.5,0.2], start = 0, end = 2
 # Output: 0.25000
 # Explanation: There are two paths from start to end, one having a probability of success = 0.2 and the other has 0.5 * 0.5 = 0.25.
+
 # Example 2:
-
-
 
 # Input: n = 3, edges = [[0,1],[1,2],[0,2]], succProb = [0.5,0.5,0.3], start = 0, end = 2
 # Output: 0.30000
+
 # Example 3:
-
-
 
 # Input: n = 3, edges = [[0,1]], succProb = [0.5], start = 0, end = 2
 # Output: 0.00000
 # Explanation: There is no path between 0 and 2.
- 
 
 # Constraints:
 
@@ -83,3 +76,24 @@ class Solution:
                 if nn in seen: continue 
                 heappush(h, (p * prob.get((n, nn), 0), nn))
         return 0
+    
+# 3 Approach
+    
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+        dist = [0] * n
+        dist[start] = 1
+        
+        for _ in range(n - 1):
+            updated = False
+            for i, (u, v) in enumerate(edges):
+                if dist[u] * succProb[i] > dist[v]:
+                    dist[v] = dist[u] * succProb[i]
+                    updated = True
+                if dist[v] * succProb[i] > dist[u]:
+                    dist[u] = dist[v] * succProb[i]
+                    updated = True
+            if not updated:
+                break
+        
+        return dist[end]
