3e34a01309dd6faffd8f7e75ca21a06f42297689
Honey Tyagi <honeyt290@gmail.com>
2024-10-05 18:59:53 +0530

    commit

--- Permutation in String.py
+++ Permutation in String.py
@@ -40,3 +40,15 @@ class Solution:
                 return True 
 
         return False
+    
+# 2 Approach
+    
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1, n2 = len(s1), len(s2)                  
+        ctr1, ctr2 = Counter(s1), Counter(s2[:n1])  
+        for i in range(n1,n2):                      
+            if ctr1 == ctr2: return True           
+            ctr2[s2[i-n1]]-= 1                     
+            ctr2[s2[i]]+= 1
+        return ctr1 == ctr2
