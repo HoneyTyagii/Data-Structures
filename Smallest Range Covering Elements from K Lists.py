ecbf3a095ef3c496f0e2aefc8526e437a1ec33c8
Honey Tyagi <honeyt290@gmail.com>
2024-10-13 21:16:05 +0530

    commit

---
+++ Smallest Range Covering Elements from K Lists.py
@@ -0,0 +1,46 @@
+# 632. Smallest Range Covering Elements from K Lists
+
+# Hard
+
+# You have k lists of sorted integers in non-decreasing order. Find the smallest range that includes at least one number from each of the k lists.
+
+# We define the range [a, b] is smaller than range [c, d] if b - a < d - c or a < c if b - a == d - c.
+
+# Example 1:
+
+# Input: nums = [[4,10,15,24,26],[0,9,12,20],[5,18,22,30]]
+# Output: [20,24]
+# Explanation: 
+# List 1: [4, 10, 15, 24,26], 24 is in range [20,24].
+# List 2: [0, 9, 12, 20], 20 is in range [20,24].
+# List 3: [5, 18, 22, 30], 22 is in range [20,24].
+
+# Example 2:
+
+# Input: nums = [[1,2,3],[1,2,3],[1,2,3]]
+# Output: [1,1] 
+
+# Constraints:
+
+# nums.length == k
+# 1 <= k <= 3500
+# 1 <= nums[i].length <= 50
+# -105 <= nums[i][j] <= 105
+# nums[i] is sorted in non-decreasing order.
+
+from typing import List
+import heapq
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        heap = [(row[0], i, 0) for i, row in enumerate(nums)]
+        heapq.heapify(heap)
+        ans = [-10**9, 10**9]
+        right = max(row[0] for row in nums)
+        while heap:
+            left, row, col = heapq.heappop(heap)
+            if right - left < ans[1] - ans[0]:
+                ans = [left, right]
+            if col + 1 == len(nums[row]):
+                return ans
+            right = max(right, nums[row][col + 1])
+            heapq.heappush(heap, (nums[row][col + 1], row, col + 1))
