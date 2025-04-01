170c728c84b36fe26e9db10d558003c14317ea69
Honey Tyagi <honeyt290@gmail.com>
2025-04-01 21:13:09 +0530

    commit

--- Solving Questions With Brainpower.py
+++ Solving Questions With Brainpower.py
@@ -50,3 +50,10 @@ class Solution:
 
             dp[i] = max(dp[i+1], pt + dp[nxt])
         return dp[0]
+
+
+# 2 Approach    
+    
+class Solution:
+    def mostPoints(self, q: List[List[int]]) -> int:
+        return (f:=cache(lambda i:i<len(q) and max(q[i][0]+f(i+q[i][1]+1),f(i+1))))(0)
