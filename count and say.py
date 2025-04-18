cf06798293463fdfe72e71f7fc468bc2798b779b
Honey Tyagi <honeyt290@gmail.com>
2025-04-18 22:32:14 +0530

    commit

--- count and say.py
+++ count and say.py
@@ -1,10 +1,39 @@
+# 38. Count and Say
+
+# Medium
+
+# The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+# countAndSay(1) = "1"
+# countAndSay(n) is the run-length encoding of countAndSay(n - 1).
+# Run-length encoding (RLE) is a string compression method that works by replacing consecutive identical characters (repeated 2 or more times) with the concatenation of the character and the number marking the count of the characters (length of the run). For example, to compress the string "3322251" we replace "33" with "23", replace "222" with "32", replace "5" with "15" and replace "1" with "11". Thus the compressed string becomes "23321511".
+
+# Given a positive integer n, return the nth element of the count-and-say sequence. 
+
+# Example 1:
+# Input: n = 4
+# Output: "1211"
+
+# Explanation:
+
+# countAndSay(1) = "1"
+# countAndSay(2) = RLE of "1" = "11"
+# countAndSay(3) = RLE of "11" = "21"
+# countAndSay(4) = RLE of "21" = "1211"
+# Example 2:
+
+# Input: n = 1
+
+# Output: "1"
+
+# Explanation:
+
+# This is the base case.
+
+# Constraints:
+
+# 1 <= n <= 30
+
 class Solution:
     def countAndSay(self, n: int) -> str:
-        sep = { "12" : "1|2", "21" : "2|1", "13" : "1|3", 
-                "31" : "3|1", "23" : "2|3", "32" : "3|2"}
-        say = "1"
-        for _ in range(n-1):
-            for w, r in sep.items(): say = say.replace(w, r)
-            say = "".join([f"{len(b)}{b[0]}" for b in say.split("|")])
-            
-        return say
+        return reduce(lambda s,_:''.join(str(len([*g]))+c for c,g in groupby(s)),range(n-1),'1')
