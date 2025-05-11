14a286f3923876e52d7910b880eda346e0b906e3
Honey Tyagi <honeyt290@gmail.com>
2025-05-11 21:28:18 +0530

    commit

--- Three Consecutive Odds.py
+++ Three Consecutive Odds.py
@@ -24,3 +24,13 @@
 class Solution:
     def threeConsecutiveOdds(self, arr):
         return "111" in "".join(map(lambda x:str(x%2), arr))
+    
+
+# 2 Approach
+    
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        for i in range(len(arr)-2):
+            if (arr[i] & 1) and (arr[i+1] & 1) and (arr[i+2] & 1):
+                return True
+        return False
