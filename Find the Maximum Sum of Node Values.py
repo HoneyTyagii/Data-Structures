067ac0595957a3769fb43217be247695f8a54200
Honey Tyagi <honeyt290@gmail.com>
2025-05-23 16:43:52 +0530

    commit

--- Find the Maximum Sum of Node Values.py
+++ Find the Maximum Sum of Node Values.py
@@ -50,3 +50,17 @@ class Solution:
         best_sum = sum(max(n, k ^ n) for n in nums)
         cnt = sum((n ^ k) > n for n in nums)  
         return best_sum - (min(abs(n - (n ^ k)) for n in nums) if cnt % 2 else 0)
+
+# 2 Approach
+    
+class Solution:
+    def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
+        n=len(nums)
+        @cache
+        def f(i, c):
+            if i==n:
+                if c==1: return -(1<<31)
+                return 0
+            x=nums[i]
+            return max(x+f(i+1, c),(x^k)+f(i+1,1-c))
+        return f(0, 0)
