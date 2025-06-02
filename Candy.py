b92cfc417019e3edb27703906ff5a7712149ebd0
Honey Tyagi <honeyt290@gmail.com>
2025-06-02 20:26:20 +0530

    commit

--- Candy.py
+++ Candy.py
@@ -53,3 +53,10 @@ class Solution:
     
 # time complexity : O(n)
 # space complexity : O(n)
+
+# 2 Approach
+    
+class Solution:
+    def candy(self, a: List[int]) -> int:
+        f = lambda a:[*accumulate(map(lt,a,a[1:]),lambda q,p:q*p+1,initial=1)]
+        return sum(map(max,f(a),f(a[::-1])[::-1]))
