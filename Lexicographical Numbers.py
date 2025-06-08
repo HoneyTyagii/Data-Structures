7762ec21bd4c939b701e65cd7624d3e656b93976
Honey Tyagi <honeyt290@gmail.com>
2025-06-08 21:46:38 +0530

    commit

--- Lexicographical Numbers.py
+++ Lexicographical Numbers.py
@@ -31,3 +31,9 @@ class Solution:
                 dfs(curr+1)   
         dfs(1)
         return output
+
+# 2 Approach
+    
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        return (f:=lambda v:sum(([u,*f(u*10)] for u in range(v,v+10-(v==1)) if u<=n),[]))(1)
