3028903954a94f1b4f270db031a2a1d30c1ec0df
Honey Tyagi <honeyt290@gmail.com>
2025-06-15 19:06:38 +0530

    commit

---
+++ Max Difference You Can Get From Changing an Integer.py
@@ -0,0 +1,40 @@
+# 1432. Max Difference You Can Get From Changing an Integer
+
+# Medium
+
+# You are given an integer num. You will apply the following steps to num two separate times:
+
+# Pick a digit x (0 <= x <= 9).
+# Pick another digit y (0 <= y <= 9). Note y can be equal to x.
+# Replace all the occurrences of x in the decimal representation of num by y.
+# Let a and b be the two results from applying the operation to num independently.
+
+# Return the max difference between a and b.
+
+# Note that neither a nor b may have any leading zeros, and must not be 0.
+
+# Example 1:
+
+# Input: num = 555
+# Output: 888
+# Explanation: The first time pick x = 5 and y = 9 and store the new integer in a.
+# The second time pick x = 5 and y = 1 and store the new integer in b.
+# We have now a = 999 and b = 111 and max difference = 888
+# Example 2:
+
+# Input: num = 9
+# Output: 8
+# Explanation: The first time pick x = 9 and y = 9 and store the new integer in a.
+# The second time pick x = 9 and y = 1 and store the new integer in b.
+# We have now a = 9 and b = 1 and max difference = 8
+ 
+
+# Constraints:
+
+# 1 <= num <= 108
+
+class Solution:
+    def maxDiff(self, v: int) -> int:
+        s = str(v)
+        return (int(s.replace((s+'_').lstrip('9')[0],'9')) - 
+            ((m:=search(r'[2-9]',s)) and int(s.replace(m[0],'01'[s[0]>'1'])) or v))
