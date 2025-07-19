c44fda27f4138662c93bff1d367e0bbaddf81780
Honey Tyagi <honeyt290@gmail.com>
2025-07-19 20:07:27 +0530

    commit

--- Remove Sub-Folders from the Filesystem.py
+++ Remove Sub-Folders from the Filesystem.py
@@ -44,3 +44,20 @@ class Solution:
       ans.append(path)
       prev = ''.join((path,'/'))
     return ans
+  
+
+# 2 Approach
+  
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        s=set()
+        ans=[]
+        for i in sorted(folder,key=len):
+            val=i[1:].split('/')
+            for v in range(len(val)):
+                if tuple(val[:v]) in s:
+                    break
+            else:
+                ans.append(i)
+                s.add(tuple(val))
+        return ans
