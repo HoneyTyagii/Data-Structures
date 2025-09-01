6bdb177c8ac658fb21143a3c662350c91b9faf27
Honey Tyagi <honeyt290@gmail.com>
2025-09-01 17:43:47 +0530

    commit

--- Maximum Average Pass Ratio.py
+++ Maximum Average Pass Ratio.py
@@ -39,3 +39,20 @@ class Solution:
             _, num, denom = heappop(classes)
             heappush(classes, ((num+1)/(denom+1) - (num+2)/(denom+2), num+1, denom+1))
         return sum(num/denom for _, num, denom in classes)/len(classes)
+    
+
+# 2 Approach
+    
+class Solution:
+    def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
+        def calc_ratio(t, p):
+            return (t - p) / (t * (t + 1))
+        ratio = [(-1 * calc_ratio(t, p), t, p) for p, t in classes]
+        heapq.heapify(ratio)
+        while extraStudents:
+            _, t, p = heapq.heappop(ratio)
+            p += 1
+            t += 1
+            heapq.heappush(ratio, (-1 * calc_ratio(t, p), t, p))
+            extraStudents -= 1
+        return sum(x[2] / x[1] if x[1] != 0 else 1 for x in ratio) / len(ratio)
