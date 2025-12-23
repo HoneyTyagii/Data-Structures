5e6f7980f8721537050dd98f7b43fe3a91768999
Honey Tyagi <honeyt290@gmail.com>
2025-12-23 21:30:05 +0530

    commit

--- Two Best Non-Overlapping Events.py
+++ Two Best Non-Overlapping Events.py
@@ -37,3 +37,26 @@
 class Solution:
     def maxTwoEvents(self, a: List[List[int]]) -> int:
         return max((M:=[*accumulate(map(itemgetter(2),(a:=sorted(a))[::-1]),max)][::-1]+[0])+[v+M[bisect_right(a,e,key=itemgetter(0))] for s,e,v in a])
+    
+# 2 Approach
+
+class Solution:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        
+        mx, ans = 0, 0
+
+        beg, end, val = zip(*events)
+        beg2ndMtg = sorted(zip(beg, val))
+        end1stMtg = iter(sorted(zip(end, val)))
+
+        end1st, val1st = next(end1stMtg)
+
+        for beg2nd, val2nd in beg2ndMtg:
+
+            while end1st < beg2nd:
+                mx = max(mx, val1st)
+                end1st, val1st = next(end1stMtg)
+
+            ans = max(ans, mx + val2nd)
+
+        return ans  
