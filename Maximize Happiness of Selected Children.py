f2e334b92d755a970b4c96fd66dc4c3e3b8656fd
Honey Tyagi <honeyt290@gmail.com>
2025-12-25 21:15:38 +0530

    commit

--- Maximize Happiness of Selected Children.py
+++ Maximize Happiness of Selected Children.py
@@ -53,3 +53,10 @@ class Solution:
             i += 1
             k -= 1
         return res
+    
+# 2 Approach
+
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(reverse=True)
+        return sum(max(0, h - i) for i, h in enumerate(happiness[:k]))
