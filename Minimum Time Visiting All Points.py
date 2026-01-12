fb6564327f4c6d7702d7042b82fa45596d5516e1
Honey Tyagi <honeyt290@gmail.com>
2026-01-12 20:48:06 +0530

    commit

--- Minimum Time Visiting All Points.py
+++ Minimum Time Visiting All Points.py
@@ -43,3 +43,22 @@ class Solution:
             total_time += max(abs(curr_pt[0] - last_pt[0]),abs(curr_pt[1] - last_pt[1]))
             last_pt = curr_pt
         return total_time
+    
+
+# 2 Approach
+
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        total_time = 0
+
+        for i in range(1, len(points)):
+            x1, y1 = points[i - 1]
+            x2, y2 = points[i]
+
+            dx = abs(x2 - x1)
+            dy = abs(y2 - y1)
+
+            total_time += max(dx, dy)
+
+        return total_time
+     
