6b4556fe72a89aeff9be1164dc7d08d94fcd388e
Honey Tyagi <honeyt290@gmail.com>
2026-02-07 20:56:08 +0530

    commit

--- Minimum Deletions to Make String Balanced.py
+++ Minimum Deletions to Make String Balanced.py
@@ -37,3 +37,9 @@ class Solution:
             else:
                 stack.append(s[i])
         return res
+    
+# 2 Approach
+
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        return min(accumulate((1-2*(c=='a') for c in s),initial=s.count('a')))
