d48fc0551b96fb9899e9e66d2f2056c8615795fa
Honey Tyagi <honeyt290@gmail.com>
2026-02-12 21:27:00 +0530

    commit

---
+++ Longest Balanced Substring I.py
@@ -0,0 +1,63 @@
+# 3713. Longest Balanced Substring I
+
+# Medium
+
+# You are given a string s consisting of lowercase English letters.
+
+# A substring of s is called balanced if all distinct characters in the substring appear the same number of times.
+
+# Return the length of the longest balanced substring of s.
+
+# Example 1:
+
+# Input: s = "abbac"
+
+# Output: 4
+
+# Explanation:
+
+# The longest balanced substring is "abba" because both distinct characters 'a' and 'b' each appear exactly 2 times.
+
+# Example 2:
+
+# Input: s = "zzabccy"
+
+# Output: 4
+
+# Explanation:
+
+# The longest balanced substring is "zabc" because the distinct characters 'z', 'a', 'b', and 'c' each appear exactly 1 time.​​​​​​​
+
+# Example 3:
+
+# Input: s = "aba"
+
+# Output: 2
+
+# Explanation:
+
+# ​​​​​​​One of the longest balanced substrings is "ab" because both distinct characters 'a' and 'b' each appear exactly 1 time. Another longest balanced substring is "ba".
+
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 1000
+# s consists of lowercase English letters.
+
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        output: int = 1
+        n: int = len(s)
+        for i in range(n):
+            hashmap: list[int] = [0] * 26
+            mx: int = 0
+            for j in range(i, n):
+                hashmap[ord(s[j]) - 97] += 1
+                mn: int = n
+                for freq in hashmap:
+                    if not freq: continue
+                    if freq < mn: mn = freq
+                    if freq > mx: mx = freq
+                if mn == mx: output = max(output, j - i + 1)
+        return output
