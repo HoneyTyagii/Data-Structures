72675cb8e6a01f624fe4a8422d6c984680a2a690
Honey Tyagi <honeyt290@gmail.com>
2026-02-14 21:14:39 +0530

    commit

--- Champagne Tower.py
+++ Champagne Tower.py
@@ -52,3 +52,11 @@ class Solution:
 
         # time complexity: O(n^2)
         # space complexity: O(n^2)
+
+
+# 2 Approach
+
+class Solution:
+    def champagneTower(self, p: int, r: int, c: int) -> float:
+        return min(1,reduce(lambda q,_:[*map(sum,
+            pairwise(max(v-1,0)/2 for v in [0]+q+[0]))],range(r),[p])[c])
