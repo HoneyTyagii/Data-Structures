d9101204b26d19ab9f9bba44851d94ed6b432ec5
Honey Tyagi <honeyt290@gmail.com>
2026-03-02 20:46:20 +0530

    commit

---
+++ Minimum Swaps to Arrange a Binary Grid.py
@@ -0,0 +1,48 @@
+# 1536. Minimum Swaps to Arrange a Binary Grid
+
+# Medium
+
+# Given an n x n binary grid, in one step you can choose two adjacent rows of the grid and swap them.
+
+# A grid is said to be valid if all the cells above the main diagonal are zeros.
+
+# Return the minimum number of steps needed to make the grid valid, or -1 if the grid cannot be valid.
+
+# The main diagonal of a grid is the diagonal that starts at cell (1, 1) and ends at cell (n, n).
+
+ 
+
+# Example 1:
+
+
+# Input: grid = [[0,0,1],[1,1,0],[1,0,0]]
+# Output: 3
+# Example 2:
+
+
+# Input: grid = [[0,1,1,0],[0,1,1,0],[0,1,1,0],[0,1,1,0]]
+# Output: -1
+# Explanation: All rows are similar, swaps have no effect on the grid.
+# Example 3:
+
+
+# Input: grid = [[1,0,0],[1,1,0],[1,1,1]]
+# Output: 0
+ 
+
+# Constraints:
+
+# n == grid.length == grid[i].length
+# 1 <= n <= 200
+# grid[i][j] is either 0 or 1
+
+
+class Solution:
+    def minSwaps(self, g: List[List[int]]) -> int:
+        res,g = 0,[len(r)-len([*takewhile(not_,r[::-1])]) for r in g]
+        for i in range(len(g)):
+            for ii,q in enumerate(g):
+                if q<=i+1: res += ii; g.pop(ii); break
+            else: return -1
+
+        return res
