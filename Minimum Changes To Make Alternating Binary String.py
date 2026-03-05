4679bf6441f6d8543271e8acf9d155dc75fdbedf
Honey Tyagi <honeyt290@gmail.com>
2026-03-05 21:15:16 +0530

    commit

--- Minimum Changes To Make Alternating Binary String.py
+++ Minimum Changes To Make Alternating Binary String.py
@@ -43,3 +43,14 @@ class Solution:
 	    startsWith1 = len(s) - startsWith0
     
 	    return min(startsWith0, startsWith1)
+	
+
+# 2 Approach
+
+class Solution:
+    def minOperations(self, s: str) -> int:
+        diff = 0
+        for i in range(len(s)):
+            if s[i] != ("1" if i % 2 == 0 else "0"):
+                diff += 1
+        return min(diff, len(s) - diff)
