ae3e5adb906b3df6afbc0b36ce4db86624fbfb90
Honey Tyagi <honeyt290@gmail.com>
2026-03-14 21:11:46 +0530

    commit

--- The k-th Lexicographical String of All Happy Strings of Length n.py
+++ The k-th Lexicographical String of All Happy Strings of Length n.py
@@ -35,3 +35,27 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
         return next(islice((s for p in product(*['abc']*n) if not search(r'(.)\1',s:=''.join(p))),k-1,k),'')
+    
+# 2 Approach
+
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        total = 3 * (2**(n - 1))
+        if k > total:
+            return ""
+
+        res = []
+        chars = ['a', 'b', 'c']
+
+        for i in range(n):
+            block = 2**(n - i - 1)
+            for c in chars:
+                if res and res[-1] == c:
+                    continue
+                if k > block:
+                    k -= block
+                else:
+                    res.append(c)
+                    break
+        
+        return "".join(res)
