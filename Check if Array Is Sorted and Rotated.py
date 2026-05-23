127c2dce20a41f35ba4a1d93333e0dc205816938
Honey Tyagi <honeyt290@gmail.com>
2026-05-23 21:26:42 +0530

    commit

--- Check if Array Is Sorted and Rotated.py
+++ Check if Array Is Sorted and Rotated.py
@@ -32,3 +32,31 @@
 class Solution:
     def check(self, a: List[int]) -> bool:
         return any(a[i:]+a[:i]==sorted(a) for i in range(len(a)))
+
+# 2 Approach
+
+from typing import List
+
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        n = len(nums)
+
+        if n <= 1:
+            return True
+
+        decend = 0
+        for i in range(1, n):
+            if nums[i] < nums[i - 1]:
+                decend += 1
+            if decend > 1:
+                return False
+        
+        # 1st element > than last element, so it's a decend
+        if nums[0] < nums[n - 1]:
+            decend += 1
+
+        return decend <= 1
+
+
+# Time Complexity   : O(N)
+# Space Complexity  : O(1)
